516837f2cd8a5e35f14176df802c7a9a870c5c94
MapleLeaf <19603573+itsMapleLeaf@users.noreply.github.com>
2020-12-05 20:31:49 -0600

    fix to encodeUriParams + docs

--- src/app/common/url.ts
+++ src/app/common/url.ts
@@ -2,8 +2,28 @@ export type UriParamsObject = {
 	[key: string]: string | number | boolean
 }
 
+/**
+ * @example
+ * // input
+ * encodeUriParams({
+ *   someString: "someString",
+ *   someNumber: 123,
+ *   empty: "",
+ *   isTrue: true, // encoded as just the key, without a value
+ *   trueString: "true",
+ *   isFalse: false, // excluded
+ *   isUndefined: undefined, // excluded
+ *   isNull: null, // excluded
+ * })
+ *
+ * // output
+ * "someString=someString&someNumber=123&empty=&isTrue&trueString=true"
+ */
 export function encodeUriParams(params: UriParamsObject) {
 	return Object.entries(params)
-		.map(([key, value]) => `${key}=${encodeURIComponent(value)}`)
+		.filter(([, value]) => value != null && value !== false)
+		.map(([key, value]) =>
+			value === true ? key : `${key}=${encodeURIComponent(value)}`,
+		)
 		.join("&")
 }
