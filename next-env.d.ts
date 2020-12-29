1b61176ab82550df638c5d404a9b22588dc8ce9a
MapleLeaf <19603573+itsMapleLeaf@users.noreply.github.com>
2020-12-29 06:08:35 -0600

    add no-arg overload for react context

--- next-env.d.ts
+++ next-env.d.ts
@@ -4,10 +4,15 @@
 
 import { css as cssImport } from "@emotion/react"
 import styledImport from "@emotion/styled"
+import "react"
 import "twin.macro"
 
 declare module "twin.macro" {
 	// The styled and css imports
 	const styled: typeof styledImport
 	const css: typeof cssImport
+}
+
+declare module "react" {
+	function createContext<T>(): Context<T | undefined>
 }
