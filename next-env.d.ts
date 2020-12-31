06d6234fb8bb5efb28569115f05f45a96bc5df3e
MapleLeaf <19603573+itsMapleLeaf@users.noreply.github.com>
2020-12-31 02:33:31 -0600

    remove unneeded deps and config

--- next-env.d.ts
+++ next-env.d.ts
@@ -1,16 +1,7 @@
 /// <reference types="next" />
 /// <reference types="next/types/global" />
-/// <reference types="@emotion/react/types/css-prop" />
 
-import { css as cssImport } from "@emotion/react"
-import styledImport from "@emotion/styled"
 import "react"
-
-declare module "twin.macro" {
-	// The styled and css imports
-	const styled: typeof styledImport
-	const css: typeof cssImport
-}
 
 declare module "react" {
 	function createContext<T>(): Context<T | undefined>
