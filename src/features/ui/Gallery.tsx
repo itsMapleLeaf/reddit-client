1f7f7a20300a28448d8c4540358b82bd5edf9d9d
MapleLeaf <19603573+itsMapleLeaf@users.noreply.github.com>
2021-01-03 17:24:49 -0600

    update twind.macro, resolve gallery error later

--- src/features/ui/Gallery.tsx
+++ src/features/ui/Gallery.tsx
@@ -1,6 +1,7 @@
 import { decrement, increment, mod } from "helpers/number"
 import { ReactNode, useState } from "react"
 import { tw } from "twind"
+import "twind.macro"
 import Icon from "./Icon"
 import { chevronLeftIcon, chevronRightIcon } from "./icons"
 
@@ -14,25 +15,23 @@ export default function Gallery<T>(props: {
 	const buttonCss = tw`bg-gray-700 rounded-full shadow pointer-events-auto`
 
 	return (
-		<div className={tw`relative h-full`}>
+		<div tw="relative h-full">
 			{currentItem != null ? props.renderItem(currentItem) : null}
 
-			<div
-				className={tw`absolute inset-0 flex flex-row items-center justify-between p-4 pointer-events-none`}
-			>
+			<div tw="absolute inset-0 flex flex-row items-center justify-between p-4 pointer-events-none">
 				<button
 					type="button"
 					className={buttonCss}
 					onClick={() => setIndex(decrement)}
 				>
-					<Icon name={chevronLeftIcon} className={tw`w-8`} />
+					<Icon name={chevronLeftIcon} tw="w-8" />
 				</button>
 				<button
 					type="button"
 					className={buttonCss}
 					onClick={() => setIndex(increment)}
 				>
-					<Icon name={chevronRightIcon} className={tw`w-8`} />
+					<Icon name={chevronRightIcon} tw="w-8" />
 				</button>
 			</div>
 		</div>
