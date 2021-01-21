bbce0418795c11e056a98f2be06a59d3ea91e667
MapleLeaf <19603573+itsMapleLeaf@users.noreply.github.com>
2021-01-20 21:10:19 -0600

    no import jsx

--- vite.config.ts
+++ vite.config.ts
@@ -3,8 +3,10 @@ import { defineConfig } from "vite"
 
 export default defineConfig({
 	esbuild: {
+		jsxInject: `import { h, Fragment } from 'preact'`,
 		jsxFactory: "h",
 		jsxFragment: "Fragment",
 	},
 	plugins: [preactRefresh()],
+	clearScreen: false,
 })
