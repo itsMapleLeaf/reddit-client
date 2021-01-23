bcacf7d08fad9da1a7c94a52ef56633b89635539
MapleLeaf <19603573+itsMapleLeaf@users.noreply.github.com>
2021-01-22 20:37:09 -0600

    fix react import 🙅‍♂️

--- src/auth-redirect.tsx
+++ src/auth-redirect.tsx
@@ -1,4 +1,4 @@
-import { useEffect } from "react"
+import { useEffect } from "preact/hooks"
 import { useMutation } from "react-query"
 import { useNavigate } from "react-router-dom"
 import { useSearchParam } from "./use-param"
