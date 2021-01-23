bd6705e7ab46b69e6d482ca8f3bf0805c16f1a72
MapleLeaf <19603573+itsMapleLeaf@users.noreply.github.com>
2021-01-22 18:06:56 -0600

    add import-meta typings

--- src/reddit-login-button.tsx
+++ src/reddit-login-button.tsx
@@ -1,13 +1,14 @@
 // import {buttonSolid} from 'features/ui/components';
 // import {encodeUriParams} from 'helpers/uri';
+import { raise } from "./helpers/error"
 import { encodeUriParams } from "./helpers/uri"
 
 const redditAuthUrl = `https://www.reddit.com/api/v1/authorize?${encodeUriParams(
 	{
-		client_id: import.meta.env.VITE_REDDIT_APP_ID,
+		client_id: import.meta.env.VITE_REDDIT_APP_ID ?? raise(),
 		response_type: `code`,
 		state: `.`,
-		redirect_uri: import.meta.env.VITE_REDDIT_APP_REDIRECT_URI,
+		redirect_uri: import.meta.env.VITE_REDDIT_APP_REDIRECT_URI ?? raise(),
 		duration: `permanent`,
 		scope: "identity read",
 	},
